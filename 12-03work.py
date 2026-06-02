9fad5264f9f885ef9541bb5c3b6334c8f784b4e2
c01dest <ilyakorshenko08@gmail.com>
2026-06-03 02:49:54 +1000

    Add basic Python examples and operations

---
+++ 12-03work.py
@@ -0,0 +1,46 @@
+# num1 = 15 #int 
+# num2 = 35
+# print(num1 + num2)
+
+# val_f1 = 5.25 #float
+# val_f2 = 1.41
+# print(val_f1 * val_f2)
+
+# text ="строка" #string
+# print(text)
+
+# items = [10,20,30,40,50] #list
+# print(items)
+
+# data = {"Key": "Value"} #dict
+# print(data["Key"])
+
+# my_tuple = (5, 6, 7) #tuple
+# print(my_tuple[0])
+
+# is_valid = True #bool
+# if is_valid == True:
+#      print("True")
+
+
+# Арифметический
+print(12 + 5)
+
+# Сравнения
+print(7 > 4)
+
+# Логический
+print(False or True)
+
+# Присваивания
+num = 20
+num += 4
+print(num)
+
+# Принадлежности
+print("кошка" in ["собака", "кошка"])
+
+# Тождественности
+val_a = [3, 4]
+val_b = val_a
+print(val_a is val_b)
